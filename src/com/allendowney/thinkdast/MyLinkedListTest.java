d1677ad1fdb8fd2e61bfe49d556c72a890009239
Anton Tobolkin <tobantal@gmail.com>
2018-12-21 23:54:16 +0700

    added MyArrayList, MyLinkedList, ProfileListAdd

---
+++ src/com/allendowney/thinkdast/MyLinkedListTest.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.allendowney.thinkdast;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ * @author downey
+ *
+ */
+public class MyLinkedListTest extends MyArrayListTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		list = new ArrayList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+
+		mylist = new MyLinkedList<Integer>();
+		mylist.addAll(list);
+	}
+
+}
---
+++ src/com/allendowney/thinkdast/ProfileListAdd.java
@@ -0,0 +1,118 @@
+package com.allendowney.thinkdast;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.jfree.data.xy.XYSeries;
+
+import com.allendowney.thinkdast.Profiler.Timeable;
+
+public class ProfileListAdd {
+	
+	private static final int startN = 4000;
+	private static final int endMillis = 1000;
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//profileArrayListAddEnd(); //.8
+		//profileArrayListAddBeginning(); //1.98
+		profileLinkedListAddBeginning(); //0.9
+		//profileLinkedListAddEnd(); //.93
+	}
+
+	/**
+	 * Characterize the run time of adding to the end of an ArrayList
+	 */
+	public static void profileArrayListAddEnd() {
+		Timeable timeable = new Timeable() {
+			List<String> list;
+
+			public void setup(int n) {
+				list = new ArrayList<String>();
+			}
+
+			public void timeMe(int n) {
+				for (int i=0; i<n; i++) {
+					list.add("a string");
+				}
+			}
+		};
+		runProfiler("ArrayList add end", timeable, startN, endMillis);
+	}
+	
+	/**
+	 * Characterize the run time of adding to the beginning of an ArrayList
+	 */
+	public static void profileArrayListAddBeginning() {
+		Timeable timeable = new Timeable() {
+			List<String> list;
+
+			public void setup(int n) {
+				list = new ArrayList<String>();
+			}
+
+			public void timeMe(int n) {
+				for (int i=0; i<n; i++) {
+					list.add(0, "a string");
+				}
+			}
+		};
+		runProfiler("ArrayList add beginning", timeable, startN, endMillis);
+	}
+
+	/**
+	 * Characterize the run time of adding to the beginning of a LinkedList
+	 */
+	public static void profileLinkedListAddBeginning() {
+		Timeable timeable = new Timeable() {
+			List<String> list;
+
+			public void setup(int n) {
+				list = new LinkedList<String>();
+			}
+
+			public void timeMe(int n) {
+				for (int i=0; i<n; i++) {
+					list.add(0, "a string");
+				}
+			}
+		};
+		runProfiler("LinkedList add beginning", timeable, startN, endMillis);
+	}
+
+	/**
+	 * Characterize the run time of adding to the end of a LinkedList
+	 */
+	public static void profileLinkedListAddEnd() {
+		Timeable timeable = new Timeable() {
+			List<String> list;
+
+			public void setup(int n) {
+				list = new LinkedList<String>();
+			}
+
+			public void timeMe(int n) {
+				for (int i=0; i<n; i++) {
+					list.add("a string");
+				}
+			}
+		};
+		runProfiler("LinkedList add end", timeable, startN, endMillis);
+	}
+
+	/**
+	 * Runs the profiles and displays results.
+	 * 
+	 * @param timeable
+	 * @param startN
+	 * @param endMillis
+	 */
+	private static void runProfiler(String title, Timeable timeable, int startN, int endMillis) {
+		Profiler profiler = new Profiler(title, timeable);
+		XYSeries series = profiler.timingLoop(startN, endMillis);
+		profiler.plotResults(series);
+	}
+}
