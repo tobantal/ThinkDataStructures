2eee68553d7f76266e2b813e2a6639843a2d29a4
Anton Tobolkin <tobantal@gmail.com>
2018-12-23 23:12:23 +0700

    added MyLinearMap

--- src/com/allendowney/thinkdast/MyLinearMap.java
+++ src/com/allendowney/thinkdast/MyLinearMap.java
@@ -14,7 +14,7 @@ import java.util.Set;
  * Implementation of a Map using a List of entries, so most
  * operations are linear time.
  *
- * @author downey
+ * @author Anton Tobolkin
  * @param <K>
  * @param <V>
  *
@@ -63,7 +63,11 @@ public class MyLinearMap<K, V> implements Map<K, V> {
 	 * @param target
 	 */
 	private Entry findEntry(Object target) {
-		// TODO: FILL THIS IN!
+		for(Entry entry : entries) {
+			if(equals(entry.getKey(),target)) {
+				return entry;
+			}
+		}
 		return null;
 	}
 
@@ -98,8 +102,8 @@ public class MyLinearMap<K, V> implements Map<K, V> {
 
 	@Override
 	public V get(Object key) {
-		// TODO: FILL THIS IN!
-		return null;
+		Entry entry = findEntry(key);
+		return entry!=null ? entry.getValue(): null;
 	}
 
 	@Override
@@ -118,7 +122,13 @@ public class MyLinearMap<K, V> implements Map<K, V> {
 
 	@Override
 	public V put(K key, V value) {
-		// TODO: FILL THIS IN!
+		Entry entry = findEntry(key);
+		if(entry!=null) {
+			V oldValue = entry.getValue();
+			entry.setValue(value);
+			return oldValue;
+		}
+		entries.add(new Entry(key,value));
 		return null;
 	}
 
@@ -131,7 +141,11 @@ public class MyLinearMap<K, V> implements Map<K, V> {
 
 	@Override
 	public V remove(Object key) {
-		// TODO: FILL THIS IN!
+		Entry entry = findEntry(key);
+		if(entry!=null) {
+			entries.remove(entry);
+			return entry.getValue();
+		}
 		return null;
 	}
 
